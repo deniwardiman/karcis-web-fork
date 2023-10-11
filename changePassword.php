3187121cbab244bbb8f185e9a1e3a93406b3c3fd
andre <asmangindaan@astra-agro.co.id>
2023-10-11 09:47:10 +0700

    fix change password

--- changePassword.php
+++ changePassword.php
@@ -7,8 +7,56 @@ include "header.php";
     <div class="container">
         <?php if(@$_GET['status'] == 'success') { ?>
             <div class="card col-6" style="border-radius: 10px; box-shadow: 0px 1px 1px 1px #FFF; margin-left: 240px;">
-                <div class="container mt-2 mb-2">
-                    <h5 style="color: #252E71;">New Password Saved Successfully</h5>
+                <div class="container mt-3 mb-2">
+                    <h5 style="color: green;">New Password Saved Successfully</h5>
+                </div>
+            </div>
+            <br><br>
+        <?php } ?>
+        <?php if(@$_GET['status'] == 'failedKurang'){?>
+            <div class="card col-6" style="border-radius: 10px; box-shadow: 0px 1px 1px 1px #FFF; margin-left: 240px;">
+                <div class="container mt-3 mb-2">
+                    <h5 style="color: red;">Password must be 8 characters</h5>
+                </div>
+            </div>
+            <br><br>
+        <?php } ?>
+        <?php if(@$_GET['status'] == 'failedBesar' || @$_GET['status'] == 'failedKecil'){?>
+            <div class="card col-6" style="border-radius: 10px; box-shadow: 0px 1px 1px 1px #FFF; margin-left: 240px;">
+                <div class="container mt-3 mb-2">
+                    <h5 style="color: red;">The password must have upper and lower case letters</h5>
+                </div>
+            </div>
+            <br><br>
+        <?php } ?>
+        <?php if(@$_GET['status'] == 'failedAngka' || @$_GET['status'] == 'failedSymbol'){?>
+            <div class="card col-6" style="border-radius: 10px; box-shadow: 0px 1px 1px 1px #FFF; margin-left: 240px;">
+                <div class="container mt-3 mb-2">
+                    <h5 style="color: red;">The password must contain a combination of numbers and symbols</h5>
+                </div>
+            </div>
+            <br><br>
+        <?php } ?>
+        <?php if(@$_GET['status'] == 'failedTidakSama'){?>
+            <div class="card col-6" style="border-radius: 10px; box-shadow: 0px 1px 1px 1px #FFF; margin-left: 240px;">
+                <div class="container mt-3 mb-2">
+                    <h5 style="color: red;">Passwords are not the same</h5>
+                </div>
+            </div>
+            <br><br>
+        <?php } ?>
+        <?php if(@$_GET['status'] == 'failedSama'){?>
+            <div class="card col-6" style="border-radius: 10px; box-shadow: 0px 1px 1px 1px #FFF; margin-left: 240px;">
+                <div class="container mt-3 mb-2">
+                    <h5 style="color: red;">The password is the same as the old password</h5>
+                </div>
+            </div>
+            <br><br>
+        <?php } ?>
+        <?php if(@$_GET['status'] == 'failedTidakSamaDatabase'){?>
+            <div class="card col-6" style="border-radius: 10px; box-shadow: 0px 1px 1px 1px #FFF; margin-left: 240px;">
+                <div class="container mt-3 mb-2">
+                    <h5 style="color: red;">The old password is wrong</h5>
                 </div>
             </div>
             <br><br>
@@ -22,15 +70,15 @@ include "header.php";
                     <form action="<?php echo $host;?>function/actChangePassword.php" method="post">
                         <div class="form-group">
                             <label>Old Password</label>
-                            <input class="form-control" type="password" name="old_password">
+                            <input class="form-control" type="password" name="old_password" required>
                         </div>
                         <div class="form-group">
                             <label>New Password</label>
-                            <input class="form-control" type="password" name="password">
+                            <input class="form-control" type="password" name="password" required>
                         </div>
                         <div class="form-group">
                             <label>Re-enter New Password</label>
-                            <input class="form-control" type="password" name="re_password">
+                            <input class="form-control" type="password" name="re_password" required>
                         </div>
                         <div class="ml-auto">
                             <button class="btn btn-primary" style="background-color: #4972E1; width: 70%; box-shadow: 0px 1px 8px 1px #4972E1; border-radius: 10px; width: 250px; margin-left: 305px; margin-top: 30px; margin-bottom: 30px;" type="submit">Save</button>
@@ -46,3 +94,5 @@ include "header.php";
 <?php
 include "footer.php";
 ?>
+
+<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
--- function/actChangePassword.php
+++ function/actChangePassword.php
@@ -8,13 +8,52 @@ $id = @$_SESSION['id'];
 if(!$id){
     header('location:'.$host.'signin.php');
 }
+if ($_POST['password'] != $_POST['re_password']) {
+    header('location:'.$host.'changePassword.php?status=failedTidakSama');
+    exit;
+}
+if (checkPassword($_POST['password']) == 'kurang') {
+    header('location:'.$host.'changePassword.php?status=failedKurang');
+    exit;
+}
+if (checkPassword($_POST['password']) == 'besar') {
+    header('location:'.$host.'changePassword.php?status=failedBesar');
+    exit;
+}
+if (checkPassword($_POST['password']) == 'kecil') {
+    header('location:'.$host.'changePassword.php?status=failedKecil');
+    exit;
+}
+if (checkPassword($_POST['password']) == 'angka') {
+    header('location:'.$host.'changePassword.php?status=failedAngka');
+    exit;
+}
+if (checkPassword($_POST['password']) == 'symbol') {
+    header('location:'.$host.'changePassword.php?status=failedSymbol');
+    exit;
+}
 
-$password = sha1($_POST['password']);
+$sqlSelectUsers = "select * from users where id = $id";
+$connSelectUsers = $conn->query($sqlSelectUsers);
+if ($connSelectUsers->num_rows > 0) {
+    $data = $connSelectUsers->fetch_assoc();
+    if (password_verify($_POST['password'].$saltKeys, $data['password'])) {
+        header('location:'.$host.'changePassword.php?status=failedSama');
+        exit;
+    }
+    if (!password_verify($_POST['old_password'].$saltKeys, $data['password'])) {
+        header('location:'.$host.'changePassword.php?status=failedTidakSamaDatabase');
+        exit;
+    }
+} else {
+    header('location:'.$host.'changePassword.php?status=failed');
+    exit;
+}
 
+$password = password_hash($_POST['password'].$saltKeys, PASSWORD_BCRYPT);
 // update data
 $user = "UPDATE users SET password = '$password' WHERE id = $id";
 $conn->query($user);
-
 
 if($conn->query($user) === FALSE){
     echo("Error description: " . mysqli_error($conn));
@@ -23,3 +62,37 @@ if($conn->query($user) === FALSE){
 header('Location: '.$host.'changePassword.php?status=success');
 
 
+function checkPassword($password){
+    if (strlen($password) < 9) {
+        return "kurang";
+    }
+    if (!hasUpperCase($password)) {
+        return "besar";
+    }
+    if (!hasLowerCase($password)) {
+        return "kecil";
+    }
+    if (!hasDigit($password)) {
+        return "angka";
+    }
+    if (!hasSymbol($password)){
+        return "symbol";
+    }
+}
+
+function hasUpperCase($str) {
+    return preg_match('/[A-Z]/', $str) ? true : false;
+}
+
+function hasLowerCase($str) {
+    return preg_match('/[a-z]/', $str) ? true : false;
+}
+
+
+function hasDigit($str) {
+    return preg_match('/\d/', $str) ? true : false;
+}
+
+function hasSymbol($str) {
+    return preg_match('/[^A-Za-z0-9]/', $str) ? true : false;
+}
